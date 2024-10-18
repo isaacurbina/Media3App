e57778c9db8d69bbd1d0a07ccc2a79a4ca64f7f2
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-10-18 16:50:42 -0500

    1. Exoplayer Media3 Stylish Video Player

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.iucoding.media3app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.iucoding.media3app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Media3
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.exoplayer.dash)
+    implementation(libs.media3.exoplayer.hls)
+    implementation(libs.media3.ui)
+}
---
+++ app/src/main/java/com/iucoding/media3app/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.iucoding.media3app
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var backButton: ImageView
+    private lateinit var forwardButton: ImageView
+    private lateinit var backwardButton: ImageView
+    private lateinit var playPauseButton: ImageView
+    private lateinit var fullscreenButton: ImageView
+    private lateinit var titleText: TextView
+    private lateinit var playerView: PlayerView
+    private lateinit var exoPlayer: ExoPlayer
+    private var isFullScreen: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playerView = findViewById(R.id.playerView)
+        exoPlayer = ExoPlayer.Builder(this).build()
+        val mediaItem = MediaItem.fromUri(VIDEO_URI)
+        playerView.player = exoPlayer
+        exoPlayer.run {
+            setMediaItem(mediaItem)
+            prepare()
+            setMediaItem(mediaItem)
+            playWhenReady = true
+            play()
+        }
+
+        backButton = playerView.findViewById(R.id.exo_back)
+        forwardButton = playerView.findViewById(R.id.exo_forward)
+        backwardButton = playerView.findViewById(R.id.exo_backward)
+        playPauseButton = playerView.findViewById(R.id.exo_play_pause_button)
+        fullscreenButton = playerView.findViewById(R.id.exo_fullscreen)
+        titleText = playerView.findViewById(R.id.exo_title)
+        titleText.text = "Big Buck Bunny"
+
+        playPauseButton.run {
+            setOnClickListener {
+                exoPlayer.playWhenReady = !exoPlayer.playWhenReady
+                setImageResource(
+                    if (exoPlayer.playWhenReady) R.drawable.pause
+                    else R.drawable.play
+                )
+            }
+        }
+        forwardButton.setOnClickListener {
+            exoPlayer.seekTo(exoPlayer.currentPosition + 10000)
+        }
+        backwardButton.setOnClickListener {
+            val num = exoPlayer.currentPosition - 10000
+            if (num < 0) exoPlayer.seekTo(0)
+            else exoPlayer.seekTo(num)
+        }
+        backButton.setOnClickListener {
+            finish()
+        }
+        fullscreenButton.setOnClickListener {
+            if (isFullScreen) {
+                titleText.visibility = View.INVISIBLE
+                fullscreenButton.setImageDrawable(
+                    ContextCompat.getDrawable(this, R.drawable.fullscreenclose)
+                )
+                supportActionBar?.show()
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                val params = playerView.layoutParams as ConstraintLayout.LayoutParams
+                params.width = MATCH_PARENT
+                params.height = (200 * resources.displayMetrics.density).toInt()
+                playerView.layoutParams = params
+                isFullScreen = false
+
+            } else {
+                titleText.visibility = View.VISIBLE
+                fullscreenButton.setImageDrawable(
+                    ContextCompat.getDrawable(this, R.drawable.fullscreenopen)
+                )
+                supportActionBar?.hide()
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                val params = playerView.layoutParams as ConstraintLayout.LayoutParams
+                params.width = MATCH_PARENT
+                params.height = MATCH_PARENT
+                playerView.layoutParams = params
+                isFullScreen = true
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        exoPlayer.play()
+    }
+
+    override fun onPause() {
+        exoPlayer.pause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        exoPlayer.release()
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val VIDEO_URI = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }
+}
